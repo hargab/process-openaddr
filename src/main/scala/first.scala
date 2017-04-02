f963bf1dc3258dfa94f857bfd115c49edaa341f1
Gabor <gabor.hargitai@gmail.com>
2017-04-02 15:38:02 +0100

    use explicit schema and save to parquet

--- src/main/scala/first.scala
+++ src/main/scala/first.scala
@@ -1,5 +1,6 @@
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.input_file_name
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 /**
   * Created by Gabor on 02/04/2017.
@@ -10,12 +11,24 @@ object first {
     .appName("process-openaddr")
     .getOrCreate()
 
+  private val basePath = "C:/Users/Gabor/IdeaProjects/resources/"
 
   def main(args: Array[String]): Unit = {
 
-    val data = spark.read.option("header", "true").csv("C:\\Users\\Gabor\\IdeaProjects\\resources\\openaddr-collected-us_northeast/summary/us/**/*.csv").withColumn("filename", input_file_name)
-    data.createOrReplaceTempView("data")
-    val stats = spark.sql("SELECT filename, COUNT(1) FROM data GROUP BY filename")
-    stats.show(100, truncate = false)
+    System.setProperty("hadoop.home.dir", "c:\\Users\\Gabor\\Hadoop\\hadoop-2.7.1\\")
+
+    val customSchema = StructType(Array(
+      StructField("count", IntegerType, nullable = true),
+      StructField("lon", DoubleType, nullable = true),
+      StructField("lat", DoubleType, nullable = true),
+      StructField("area", StringType, nullable = true)))
+
+    val data = spark.read.option("header", "true").schema(customSchema).csv(basePath + "openaddr-collected-us_northeast/summary/us/**/*.csv").withColumn("filename", input_file_name)
+    //data.createOrReplaceTempView("data")
+    //val stats = spark.sql("SELECT filename, COUNT(1) FROM data GROUP BY filename")
+
+    data.show(100, truncate = false)
+    data.printSchema
+    data.write.mode(SaveMode.Overwrite).parquet(basePath + "summary.parquet")
   }
 }
