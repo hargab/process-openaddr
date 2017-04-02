490abd46613c4c49c511d30fa3d390f0e22eb370
Gabor <gabor.hargitai@gmail.com>
2017-04-02 17:55:32 +0100

    read and write all us_northeast data

--- src/main/scala/first.scala
+++ src/main/scala/first.scala
@@ -17,6 +17,10 @@ object first {
 
     System.setProperty("hadoop.home.dir", "c:\\Users\\Gabor\\Hadoop\\hadoop-2.7.1\\")
 
+    loadAndSaveFull()
+  }
+
+  def loadAndSaveSummary(): Unit = {
     val customSchema = StructType(Array(
       StructField("count", IntegerType, nullable = true),
       StructField("lon", DoubleType, nullable = true),
@@ -24,11 +28,37 @@ object first {
       StructField("area", StringType, nullable = true)))
 
     val data = spark.read.option("header", "true").schema(customSchema).csv(basePath + "openaddr-collected-us_northeast/summary/us/**/*.csv").withColumn("filename", input_file_name)
-    //data.createOrReplaceTempView("data")
-    //val stats = spark.sql("SELECT filename, COUNT(1) FROM data GROUP BY filename")
-
     data.show(100, truncate = false)
     data.printSchema
     data.write.mode(SaveMode.Overwrite).parquet(basePath + "summary.parquet")
+  }
+
+  def loadAndSaveFull(): Unit = {
+    val customSchema = StructType(Array(
+      StructField("lon", new DecimalType(10, 7), nullable = true),
+      StructField("lat", new DecimalType(10, 7), nullable = true),
+      StructField("number", StringType, nullable = true),
+      StructField("street", StringType, nullable = true),
+      StructField("unit", StringType, nullable = true),
+      StructField("city", StringType, nullable = true),
+      StructField("district", StringType, nullable = true),
+      StructField("region", StringType, nullable = true),
+      StructField("postcode", IntegerType, nullable = true),
+      StructField("id", IntegerType, nullable = true),
+      StructField("hash", StringType, nullable = true)
+    ))
+
+    val path = "openaddr-collected-us_northeast/us/**/*.csv"
+    val data = spark.read.option("header", "true").option("treatEmptyValuesAsNulls", "true").option("mode", "DropMalformed").schema(customSchema).csv(basePath + path).withColumn("filename", input_file_name)
+    data.write.mode(SaveMode.Overwrite).parquet(basePath + "us_northeast.parquet")
+  }
+
+  def isNumber(s: String): Boolean = {
+    try {
+      s.toDouble
+      return true
+    } catch {
+      case _ => return false
+    }
   }
 }
